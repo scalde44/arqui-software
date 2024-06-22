8cc7023238f5a00a21428fab9a20293f3e12fc6c
Steven Calderon <steven.calderon@sofka.com.co>
2024-06-22 10:48:02 -0500

    Commit inicial

---
+++ ejercicio-capas/src/main/java/edu/co/usbcali/arqui/ejercicio/ArquitecturaCapasApplication.java
@@ -0,0 +1,13 @@
+package edu.co.usbcali.arqui.ejercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquitecturaCapasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquitecturaCapasApplication.class, args);
+	}
+
+}
---
+++ ejercicio-capas/src/test/java/edu/co/usbcali/arqui/ejercicio/ArquitecturaCapasApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.co.usbcali.arqui.ejercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquitecturaCapasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
