41d7a8dc94ba3115b06e8c368e5c8656a3396617
Steven Calderon <steven.calderon@sofka.com.co>
2024-07-05 00:21:38 -0500

    Se crea utilitario para los mensajes

---
+++ ejercicio-cliente-servidor/src/main/java/co/edu/usbcali/arqui/ejerciciocliente/MensajesUtils.java
@@ -0,0 +1,12 @@
+package co.edu.usbcali.arqui.ejerciciocliente;
+
+public final class MensajesUtils {
+    private MensajesUtils() {
+    }
+
+    public static String SALUDO_SERVER = "hello server";
+    public static String SALUDO_CLIENTE = "hello client";
+    public static String DESPEDIDA_SERVER = "bye";
+    public static String DESPEDIDA_CLIENTE = "bye";
+
+}
