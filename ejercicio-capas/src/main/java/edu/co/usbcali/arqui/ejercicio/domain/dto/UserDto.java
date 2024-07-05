81858e0d6aa454a6c5ede9b72ca21f31bef13216
Steven Calderon <steven.calderon@sofka.com.co>
2024-07-04 23:25:19 -0500

    Se crea Dto

---
+++ ejercicio-capas/src/main/java/edu/co/usbcali/arqui/ejercicio/domain/dto/UserDto.java
@@ -0,0 +1,16 @@
+package edu.co.usbcali.arqui.ejercicio.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private String id;
+    private String name;
+    private String email;
+}
